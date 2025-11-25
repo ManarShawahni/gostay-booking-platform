0082327e68ed1fd292d0521fa13bf7ab469bae7b
ManarShawahni <manarshawahnii@gmail.com>
2025-11-25 04:50:01 +0200

    feat(home): implement Search Widget UI with icons, grid layout and styling [GOS-42]

--- src/components/features/home/SearchWidget/SearchWidget.tsx
+++ src/components/features/home/SearchWidget/SearchWidget.tsx
@@ -1,3 +1,65 @@
-export const SearchWidget = () => {
-  return <div>Search Widget Placeholder</div>;
+import React from "react";
+import styles from "./SearchWidget.module.css";
+import { MagnifyingGlassIcon, MapPinIcon, CalendarIcon, UserGroupIcon } from "@heroicons/react/24/outline";
+
+export const SearchWidget: React.FC = () => {
+  return (
+    <div className={styles.widget}>
+
+      {/* DESTINATION */}
+      <div className={styles.field}>
+        <label className={styles.label}>
+          <MapPinIcon className={styles.icon} />
+          Where to?
+        </label>
+        <input type="text" placeholder="Where are you going?" className={styles.input} />
+      </div>
+
+      <div className={styles.divider}></div>
+
+      {/* CHECK-IN */}
+      <div className={styles.field}>
+        <label className={styles.label}>
+          <CalendarIcon className={styles.icon} />
+          Check-in
+        </label>
+        <input type="date" className={styles.input} />
+      </div>
+
+      <div className={styles.divider}></div>
+
+      {/* CHECK-OUT */}
+      <div className={styles.field}>
+        <label className={styles.label}>
+          <CalendarIcon className={styles.icon} />
+          Check-out
+        </label>
+        <input type="date" className={styles.input} />
+      </div>
+
+      <div className={styles.divider}></div>
+
+      {/* GUESTS */}
+      <div className={styles.field}>
+        <label className={styles.label}>
+          <UserGroupIcon className={styles.icon} />
+          Guests
+        </label>
+
+        <div className={styles.selectWrapper}>
+          <select className={styles.input}>
+            {[1,2,3,4,5].map(n => (
+              <option key={n}>{n} {n > 1 ? "Adults" : "Adult"}</option>
+            ))}
+          </select>
+        </div>
+      </div>
+
+      {/* SEARCH BUTTON */}
+      <button className={styles.searchBtn}>
+        <MagnifyingGlassIcon className={styles.searchIcon} />
+      </button>
+
+    </div>
+  );
 };
